03fc7169b73d0a589ee4c2bbd48d59d04b6ad106
YinFreeman <laordendelassombras@protonmail.com>
2022-07-15 11:00:57 +0200

    Primer comit

---
+++ controllers/usuarios.js
@@ -0,0 +1,41 @@
+const {response} = require('express')
+
+const usuariosGet = (req, res = response) => {
+    const query = req.query
+    res.json({
+        msg: 'get API - controlador',
+        query
+    })
+}
+
+const usuariosPut = (req, res = response) => {
+    const id = req.params.id
+    res.json({
+        "msg": "put API - controlador",
+        id
+        
+    })
+}
+
+const usuariosPost = (req, res = response) => {
+    const {nombre,edad} = req.body
+
+    res.json({
+        "msg": "post API",
+        nombre,edad
+    })
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        "msg": "delete API"
+    })
+}
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete
+}
